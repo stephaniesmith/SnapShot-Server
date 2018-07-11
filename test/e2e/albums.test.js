1c8462e0114617f2ab6d2e0ab157d36626887cc2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-07-10 21:08:33 -0700

    delete album by id

--- test/e2e/albums.test.js
+++ test/e2e/albums.test.js
@@ -68,4 +68,17 @@ describe('Album E2E API', () => {
             });
     });
 
+    it('deletes an album by id', () => {
+        return request.delete(`/api/albums/${kitten._id}`)
+            .set('Authorization', token)
+            .then(({ body }) => {
+                assert.deepEqual(body, kitten);
+                return request.get(`/api/albums/${kitten._id}`)
+                    .set('Authorization', token)
+                    .then(({ error }) => {
+                        assert.deepEqual(error.status, 404);
+                    });
+            });
+    });
+
 });
